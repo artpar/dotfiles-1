b8477db51cf5d5ee12a6f2e6d57180ecc0ea5435
Parth Mudgal <artpar@gmail.com>
2014-11-04 19:12:27 +0530

    added alises for fasd

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,6 +12,19 @@ elif [[ $unamestr == 'Darwin' ]]; then
   platform='darwin'
 fi
 
+#Fasd aliases
+
+alias a='fasd -a'        # any
+alias s='fasd -si'       # show / search / select
+alias d='fasd -d'        # directory
+alias f='fasd -f'        # file
+alias sd='fasd -sid'     # interactive directory selection
+alias sf='fasd -sif'     # interactive file selection
+alias z='fasd_cd -d'     # cd, same functionality as j in autojump
+alias zz='fasd_cd -d -i' # cd with interactive selection
+
+
+
 # YADR support
 alias yav='yadr vim-add-plugin'
 alias ydv='yadr vim-delete-plugin'
